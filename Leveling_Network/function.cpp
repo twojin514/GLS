492dac459c96409bbc8a08fca470188d5f376f99
twojin514 <twojin514@gmail.com>
2024-03-18 21:26:27 +0900

    초기완료

--- Leveling_Network/function.cpp
+++ Leveling_Network/function.cpp
@@ -71,62 +71,174 @@ void ReadInputBMFile(std::string FileName, std::vector<Benchmark>& benchmarks_da
     benchmarks_data = vetor_benchmarks;
 }
 
-void PrintInputData(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data)
+void PrintLevelingProgram(std::ofstream& outfile, std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, std::vector<std::vector<int32_t>>& a_matrix, std::vector<std::vector<std::string>>& x_matrix, std::vector<std::vector<double>>& l_matrix, std::vector<std::vector<double>>& w_matrix, std::vector<std::vector<double>>& atwa_matrix, std::vector<std::vector<double>>& atwl_matrix)
 {
-    printf("\nLine, ID(To), ID(From), Elevation_Difference, Standard Deviation(m)\n");
+    cv::Mat cv_a_matrix, cv_l_matrix, cv_w_matrix, cv_atwa_matrix, cv_atwl_matrix;
+
+    VectoMatrix(atwa_matrix, cv_atwa_matrix);
+    VectoMatrix(atwl_matrix, cv_atwl_matrix);
+    VectoMatrix(w_matrix, cv_w_matrix);
+    VectoMatrix(a_matrix, cv_a_matrix);
+    VectoMatrix(l_matrix, cv_l_matrix);
+    
+
+
+    outfile << "**********************************************************************************************\n";
+    outfile << "************************* 고급 조정계산론 - 수준망 조정프로그램 ***********************\n";
+    outfile << "**********************************************************************************************\n";
+    outfile << "********************************************** 작성자 : 김홍진 *****************************\n";
+
+    time_t timer;
+    struct tm curr_timer;
+    timer = time(NULL);
+
+    outfile << "********************************************** ";
+    if (localtime_s(&curr_timer, &timer) == 0) {
+        outfile << "조정시각 : " << curr_timer.tm_year + 1900 << "년 " << curr_timer.tm_mon + 1 << "월 " << curr_timer.tm_mday << "일"<< curr_timer.tm_hour << "시 " << curr_timer.tm_min << "분 " << curr_timer.tm_sec << "초\n";
+    }
+    else {
+        outfile << "시간 정보를 가져오는 데 실패했습니다." << std::endl;
+    }
+    outfile << "**********************************************************************************************\n\n";
+
+
+    outfile << "\n*************************************** 1.  입력 자료 ***************************************\n";
+    outfile << "************************************* Line Input Data **************************************\n";
+    outfile << "\nLine\tID(To)\tID(From)\tElevation_Difference\tStandard Deviation(m)\n";
     for (int i = 0; i < lines_data.size(); i++)
     {
-		printf("%d, %s, %s, %f, %f\n", lines_data[i].line_number, lines_data[i].id_to.c_str(), lines_data[i].id_from.c_str(), lines_data[i].elevation_difference, lines_data[i].standard_deviation);
-	}
+        outfile << lines_data[i].line_number << "\t" << lines_data[i].id_to << "\t" << lines_data[i].id_from << "\t" << lines_data[i].elevation_difference << "\t" << lines_data[i].standard_deviation << "\n";
+    }
 
-
-	printf("\nNumber, ID, Elevation_Difference, Standard Deviation(m)\n");
+    outfile<<"\nNumber\tID\tElevation_Difference\tStandard Deviation(m)\n";
     for (int i = 0; i < benchmarks_data.size(); i++)
     {
-		printf("%d, %s, %f, %f\n", benchmarks_data[i].benchmark_number, benchmarks_data[i].id.c_str(), benchmarks_data[i].elevation, benchmarks_data[i].standard_deviation);
+        outfile << benchmarks_data[i].benchmark_number << "\t" << benchmarks_data[i].id << "\t" << benchmarks_data[i].elevation << "\t" << benchmarks_data[i].standard_deviation << "\n";
+    }
+
+    outfile << "\n************************************ 2. 수준망 조정 시작 ***********************************\n\n";
+    outfile << "측점의 수 : " << x_matrix.size() << "\t\t"<<"관측 수 : "<<lines_data.size()+benchmarks_data.size()<<"\n\n";
+    outfile << "**************************************** Matrix 출력 ***************************************\n\n";
+
+    outfile<<"[ X Matrix ]\n";
+    for (int i = 0; i < x_matrix.size(); i++)
+    {
+		outfile << x_matrix[i][0] << "\n";
 	}
 
-    printf("\n");
+    outfile << "\n[ A Matrix ]\n";
+    for (int i = 0; i < a_matrix.size(); i++)
+    {
+        for (int j = 0; j < a_matrix[i].size(); j++)
+        {
+			outfile << a_matrix[i][j] << " ";
+		}
+		outfile << "\n";
+	}
+
+    outfile << "\n[ L Matrix ]\n";
+    for (int i = 0; i < l_matrix.size(); i++)
+    {
+		outfile << l_matrix[i][0] << "\n";
+	}
+
+	outfile << "\n[ W Matrix ]\n";
+    for (int i = 0; i < w_matrix.size(); i++)
+    {
+        for (int j = 0; j < w_matrix[i].size(); j++)
+        {
+			outfile << w_matrix[i][j] << " ";
+		}
+		outfile << "\n";
+	}
+
+	outfile << "\n[ ATWA Matrix ]\n";
+    for (int i = 0; i < atwa_matrix.size(); i++)
+    {
+        for (int j = 0; j < atwa_matrix[i].size(); j++)
+        {
+			outfile << atwa_matrix[i][j] << " ";
+		}
+		outfile << "\n";
+	}
+
+	outfile << "\n[ ATWL Matrix ]\n";
+    for (int i = 0; i < atwl_matrix.size(); i++)
+    {
+		outfile << atwl_matrix[i][0] << "\n";
+	}
 
 
+
+	outfile << "\n\n*************************************** 3. 조정결과 ***************************************\n\n";
+
+    cv::Mat cv_inv_atwa_matrix = cv_atwa_matrix.inv();
+    cv::Mat calculate_x_matrix = cv_inv_atwa_matrix * cv_atwl_matrix;
+
+    outfile << "[ X Matrix ]\n";
+    for (int i = 0; i < x_matrix.size(); i++)
+    {
+        for (int j = 0; j < x_matrix[i].size(); j++)
+        {
+            outfile << x_matrix[i][j] << " : " << calculate_x_matrix.at<double>(i, j) << "\n";
+        }
+    }
+
+    outfile<< "\n[ V Matrix ]\n";
+    cv_a_matrix.convertTo(cv_a_matrix, CV_64F);
+    cv::Mat cv_v_matrix = cv_a_matrix* calculate_x_matrix - cv_l_matrix;
+    for (int i = 0; i < cv_v_matrix.rows; i++)
+    {
+		outfile << cv_v_matrix.at<double>(i, 0) << "\n";
+	}
+
+	outfile << "\n[ 정확도 ]\n";
+	cv::Mat cv_q_matrix = cv_v_matrix.t() * cv_w_matrix * cv_v_matrix;
+	outfile << sqrt(cv_q_matrix.at<double>(0, 0) / (lines_data.size() + benchmarks_data.size() - x_matrix.size())) << "\n";
 }
 
 void SortInputData(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int& point_number, std::vector<std::string>& point_names)
 {
-    std::vector<std::string> point_names_temp;
+	std::vector<std::string> point_names_temp;
 
-    // Collect point names from lines_data and benchmarks_data
-    for (const auto& line : lines_data) {
-        point_names_temp.push_back(line.id_from);
-        point_names_temp.push_back(line.id_to);
-    }
-    for (const auto& benchmark : benchmarks_data) {
-        point_names_temp.push_back(benchmark.id);
-    }
+	// Collect point names from lines_data and benchmarks_data
+	for (const auto& line : lines_data) {
+		point_names_temp.push_back(line.id_from);
+		point_names_temp.push_back(line.id_to);
+	}
+	for (const auto& benchmark : benchmarks_data) {
+		point_names_temp.push_back(benchmark.id);
+	}
 
-    // Sort point_names_temp
-    std::sort(point_names_temp.begin(), point_names_temp.end());
+	// Sort point_names_temp
+	std::sort(point_names_temp.begin(), point_names_temp.end());
 
-    // Remove duplicates
-    point_names_temp.erase(std::unique(point_names_temp.begin(), point_names_temp.end()), point_names_temp.end());
+	// Remove duplicates
+	point_names_temp.erase(std::unique(point_names_temp.begin(), point_names_temp.end()), point_names_temp.end());
 
-    // Update point_number and point_names
-    point_number = point_names_temp.size();
-    point_names = point_names_temp;
+	// Update point_number and point_names
+	point_names = point_names_temp;
+	point_number = point_names_temp.size();
 
-    // Print sorted point names
-    for (const auto& str : point_names_temp) {
-        std::cout << str << std::endl;
-    }
+	// Print sorted point names
+	//for (const auto& str : point_names_temp) {
+	//	std::cout << str << std::endl;
+	//}
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 void SetXMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& n_measurement_point, std::vector<std::string>& point_names, std::vector<std::vector<std::string>>& x_matrix)
 {
-    std::vector<std::vector<std::string>> new_x_matrix (n_measurement_point, std::vector<std::string>(1));
+    std::vector<std::vector<std::string>> new_x_matrix(n_measurement_point, std::vector<std::string>(1));
     for (int i = 0; i < n_measurement_point; ++i)
     {
         new_x_matrix[i][0] = point_names[i];
-	}
+    }
 
     x_matrix = new_x_matrix;
 
@@ -136,13 +248,13 @@ void SetXMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmark
 void SetAMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& m_observation_point, int32_t& n_measurement_point, std::vector<std::vector<std::string>>& x_matrix, std::vector<std::vector<std::int32_t>>& a_matrix)
 {
     std::vector<std::vector<std::int32_t>> new_a_matrix(m_observation_point, std::vector<std::int32_t>(n_measurement_point));
-    
+
     for (int i = 0; i < lines_data.size(); ++i) {
         for (int j = 0; j < n_measurement_point; ++j) {
             if (lines_data[i].id_from == x_matrix[j][0]) {
                 new_a_matrix[i][j] = -1;
             }
-            
+
             else if (lines_data[i].id_to == x_matrix[j][0]) {
                 new_a_matrix[i][j] = 1;
             }
@@ -160,22 +272,22 @@ void SetAMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmark
         {
             if (benchmarks_data[i - lines_data.size()].id == x_matrix[j][0])
             {
-				new_a_matrix[i][j] = 1;
-			}
+                new_a_matrix[i][j] = 1;
+            }
             else
             {
-				new_a_matrix[i][j] = 0;
-			}
-		}
-	}
+                new_a_matrix[i][j] = 0;
+            }
+        }
+    }
 
 
     a_matrix = new_a_matrix;
 }
 
-void SetLMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& m_observation_point, std::vector<std::vector<float>>& l_matrix)
+void SetLMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& m_observation_point, std::vector<std::vector<double>>& l_matrix)
 {
-    	std::vector<std::vector<float>> new_l_matrix(m_observation_point, std::vector<float>(1));
+    	std::vector<std::vector<double>> new_l_matrix(m_observation_point, std::vector<double>(1));
         for (int i = 0; i < lines_data.size(); ++i)
         {
 		new_l_matrix[i][0] = lines_data[i].elevation_difference;
@@ -186,11 +298,10 @@ void SetLMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmark
 		new_l_matrix[i][0] = benchmarks_data[i - lines_data.size()].elevation;
 	}
 
-       // printf("L Matrix\n");
-         for (int i = 0; i < m_observation_point; ++i)
-         {
-             printf("%f\n", new_l_matrix[i][0]);
-         }
+         //for (int i = 0; i < m_observation_point; ++i)
+         //{
+         //    printf("%f\n", new_l_matrix[i][0]);
+         //}
 
 
 	l_matrix = new_l_matrix;
@@ -223,5 +334,66 @@ void SetWMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmark
 
 	w_matrix = new_w_matrix;
 }
+
+void SetATWAMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& n_measurement_point, std::vector<std::vector<int32_t>>& a_matrix, std::vector<std::vector<double>>& w_matrix, std::vector<std::vector<double>>& atwa_matrix)
+{
+    std::vector<std::vector<double>> new_atwa_matrix(n_measurement_point, std::vector<double>(n_measurement_point));
+
+    for (int i = 0; i < n_measurement_point; ++i)
+    {
+        for (int j = 0; j < n_measurement_point; ++j)
+        {
+			new_atwa_matrix[i][j] = 0;
+            for (int k = 0; k < lines_data.size() + benchmarks_data.size(); ++k)
+            {
+				new_atwa_matrix[i][j] += a_matrix[k][i] * w_matrix[k][k] * a_matrix[k][j];
+			}
+		}
+	}
+    
+	atwa_matrix = new_atwa_matrix;
+}
+
+void SetATWLMatrix(std::vector<Line>& lines_data, std::vector<Benchmark>& benchmarks_data, int32_t& n_measurement_point, std::vector<std::vector<int32_t>>& a_matrix, std::vector<std::vector<double>>& w_matrix, std::vector<std::vector<double>>& l_matrix, std::vector<std::vector<double>>& atwl_matrix)
+{
+    std::vector<std::vector<double>> new_atwl_matrix(n_measurement_point, std::vector<double>(1));
+
+    for (int i = 0; i < n_measurement_point; ++i)
+    {
+		new_atwl_matrix[i][0] = 0;
+        for (int j = 0; j < lines_data.size() + benchmarks_data.size(); ++j)
+        {
+			new_atwl_matrix[i][0] += a_matrix[j][i] * w_matrix[j][j] * l_matrix[j][0];
+		}
+	}
+
+	atwl_matrix = new_atwl_matrix;
+}
+
+void VectoMatrix(std::vector<std::vector<double>>& matrix, cv::Mat& mat)
+{
+    mat = cv::Mat(matrix.size(), matrix[0].size(), CV_64F);
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[0].size(); j++)
+        {
+			mat.at<double>(i, j) = matrix[i][j];
+		}
+	}
+}
+
+void VectoMatrix(std::vector<std::vector<int32_t>>& matrix, cv::Mat& mat)
+{
+    mat = cv::Mat(matrix.size(), matrix[0].size(), CV_32S);
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[0].size(); j++)
+        {
+            mat.at<int32_t>(i, j) = (matrix[i][j]);
+        }
+    }
+}
+
+
 
 
--- Leveling_Network/main.cpp
+++ Leveling_Network/main.cpp
@@ -1,5 +1,7 @@
-﻿#include <iostream>
-#include "function.h"
+﻿#include "function.h"
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 
 int main(int argc, char* argv[])
 {
@@ -17,53 +19,75 @@ int main(int argc, char* argv[])
 
 
 	/*********************************************************************
-	 1.  입력 자료 읽기 및 출력
+	 1.  입력 자료 읽기
 	**********************************************************************/
 
-	std::printf("************************************************************************\n");
-	std::printf("************************* 수준망 조정프로그램 **************************\n");
-	std::printf("************************************************************************\n");
-
-	outfile << "************************************************************************\n";
-	outfile << "************************ 수준망 조정프로그램 ***********************\n";
-	outfile << "************************************************************************\n";
 
 	std::vector<Line> lines;
 	std::vector<Benchmark> benchmarks;
 
 	ReadInputLineFile(input_line_data, lines);
 	ReadInputBMFile(input_bm_data, benchmarks);
-	PrintInputData(lines, benchmarks);
 
 	/*********************************************************************
-	 2. 측점 논리적 순서 정렬
-	**********************************************************************/
-
+	2. 수준망 조정 시작
+	*********************************************************************/
+	// 2.0 측점 논리적 순서 정렬
 	int32_t point_num;
 	std::vector<std::string> point_names;
 
 	SortInputData(lines, benchmarks, point_num, point_names);
-		
 
-	/*********************************************************************
-	3.  수준망 조정
-	*********************************************************************/
-	// 3.1 Matrix 설정
+	// 2.1 Matrix 설정
 	int32_t n_measurement_point = point_num;
 	int32_t m_observation_point = lines.size()+benchmarks.size();
 
 	std::vector<std::vector<std::string>> x_matrix;
-	SetXMatrix(lines, benchmarks, point_num, point_names, x_matrix);
-
 	std::vector<std::vector<int32_t>> a_matrix;
-	SetAMatrix(lines, benchmarks, m_observation_point, n_measurement_point,x_matrix ,a_matrix);
-
-	std::vector<std::vector<float>> l_matrix;
-	SetLMatrix(lines, benchmarks, m_observation_point, l_matrix);
-
+	std::vector<std::vector<double>> l_matrix;
 	std::vector<std::vector<double>> w_matrix;
-	SetWMatrix(lines, benchmarks, m_observation_point, w_matrix);
+	std::vector<std::vector<double>> atwa_matrix;
+	std::vector<std::vector<double>> atwl_matrix;
 
+	SetXMatrix(lines, benchmarks, point_num, point_names, x_matrix);
+	SetAMatrix(lines, benchmarks, m_observation_point, n_measurement_point,x_matrix ,a_matrix);
+	SetLMatrix(lines, benchmarks, m_observation_point, l_matrix);
+	SetWMatrix(lines, benchmarks, m_observation_point, w_matrix);
+	SetATWAMatrix(lines, benchmarks, n_measurement_point, a_matrix, w_matrix, atwa_matrix);
+	SetATWLMatrix(lines, benchmarks, n_measurement_point, a_matrix, w_matrix, l_matrix, atwl_matrix);
+	
+	// 2.2 Matrix 출력
+	PrintLevelingProgram(outfile, lines, benchmarks, a_matrix, x_matrix, l_matrix, w_matrix, atwa_matrix, atwl_matrix);
+	cv::Mat cv_x_matrix, cv_a_matrix, cv_l_matrix, cv_w_matrix, cv_atwa_matrix, cv_atwl_matrix, cv_inv_atwa_matrix;
+
+	cv::Mat mat = cv::Mat(a_matrix.size(), a_matrix[0].size(), CV_32F);
+	for (int i = 0; i < mat.rows; ++i) {
+		for (int j = 0; j < mat.cols; ++j) {
+			mat.at<float>(i, j) = a_matrix[i][j];
+		}
+	}
+
+	cv::Mat mat2=cv::Mat(w_matrix.size(), w_matrix[0].size(), CV_32F);
+	for (int i = 0; i < mat2.rows; ++i) {
+		for (int j = 0; j < mat2.cols; ++j) {
+			mat2.at<float>(i, j) = w_matrix[i][j];
+		}
+	}
+	cv::Mat mat3 = cv::Mat(l_matrix.size(), l_matrix[0].size(), CV_32F);
+	for (int i = 0; i < mat3.rows; ++i) {
+		for (int j = 0; j < mat3.cols; ++j) {
+			mat3.at<float>(i, j) = l_matrix[i][j];
+		}
+	}
+
+	std::cout<<(mat.t()*mat2*mat)<<std::endl;
+	std::cout<<(mat.t()*mat2*mat3)<<std::endl;
+	std::cout<<(mat.t()*mat2*mat).inv()<<std::endl;
+	std::cout<<(mat.t()*mat2*mat).inv()*(mat.t()*mat2*mat3)<<std::endl;
+
+
+
+	outfile.close();
 
 	return 0;
 }
